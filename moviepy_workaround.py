e1c573eac0399da765602facebed5e605d6255c6
Mikhail <mixandoma@ya.ru>
2018-12-15 10:47:01 +0500

    cut_video functional

--- moviepy_workaround.py
+++ moviepy_workaround.py
@@ -1,6 +1,7 @@
 from moviepy.editor import VideoFileClip
 
-my_video = VideoFileClip("/home/mishkadoma/Desktop/test_sample.ts")
+# my_video = VideoFileClip("/home/mishkadoma/Desktop/test_sample.ts")
+my_video = VideoFileClip("/home/mishkadoma/Desktop/test_sample_2.mp4")
 
 
 def hours_in_video(video):
@@ -8,5 +9,16 @@ def hours_in_video(video):
     return "%.2f" % hoursinvideo
 
 
+def cut_video(input_video, output_video, start, stop):
+    result = input_video.subclip(start, stop)
+    result.write_videofile("{}.mov".format(output_video),
+                           codec="libx264", fps=my_video.fps)
+    result.close()
+    return result
+
+
 print("Video length is {} minutes and it is {} fps".
       format(hours_in_video(my_video), my_video.fps))
+
+
+print("".format(cut_video(my_video, "cutted_video", 33, 110)))
