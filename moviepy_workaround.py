5157a586f741b9a1deb104c5e43689f8812d27fc
Mikhail <mixandoma@ya.ru>
2018-12-15 23:50:04 +0500

    text to video render testing

--- moviepy_workaround.py
+++ moviepy_workaround.py
@@ -1,4 +1,4 @@
-from moviepy.editor import VideoFileClip
+from moviepy.editor import VideoFileClip, TextClip
 
 # my_video = VideoFileClip("/home/mishkadoma/Desktop/test_sample.ts")
 my_video = VideoFileClip("/home/mishkadoma/Desktop/test_sample_2.mp4")
@@ -9,16 +9,30 @@ def hours_in_video(video):
     return "%.2f" % hoursinvideo
 
 
-def cut_video(input_video, output_video, start, stop):
+def cut_video(input_video, start, stop, output_video="cutted_video"):
     result = input_video.subclip(start, stop)
-    result.write_videofile("{}.mov".format(output_video),
+    result.write_videofile("{}.mp4".format(output_video),
                            codec="libx264", fps=my_video.fps)
     result.close()
-    return result
+
+
+def make_gif(input_video, start, stop, speed, output_gif="output.gif"):
+    input_video.subclip(start, stop).speedx(speed).to_gif(output_gif)
 
 
 print("Video length is {} minutes and it is {} fps".
       format(hours_in_video(my_video), my_video.fps))
 
+#
+# cut_video(my_video, 33, 110, "cutted_video")
+# make_gif(my_video, 10, 12, 2)
 
-print("".format(cut_video(my_video, "cutted_video", 33, 110)))
+my_video.save_frame("frame.jpeg")
+
+
+# making clip out of the text
+
+clip = TextClip("Sample text", font='Courier', fontsize=50, color="red")
+clip.save_frame("text.png")
+clip.duration = 10
+clip.write_videofile("text.mov", codec="libx264", fps=10)
